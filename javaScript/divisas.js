8fa1b554ee7d5b18ffbd59a4211e061283af183a
Jesus Suarez <jesus_suarez_11@outlook.com>
2023-11-22 00:59:52 -0500

    Creación de Archivos, ejercicios 1 y 2 Terminados

---
+++ javaScript/divisas.js
@@ -0,0 +1,20 @@
+//Convertir de Pesos a Dolares
+let pesos = document.getElementById('pesos')
+pesos.addEventListener('keyup', pesosDolares)
+
+function pesosDolares() {
+    let pesos = document.getElementById('pesos').value
+    let conversion = document.getElementById('dolares')
+    conversion.value = (pesos / 4050).toFixed(2);
+}
+
+//Convertir de Dolares a Pesos
+let dolares = document.getElementById('dolares')
+dolares.addEventListener('keyup', dolaresPesos)
+
+function dolaresPesos() {
+    let dolares = document.getElementById('dolares').value
+    let conversion = document.getElementById('pesos')
+    conversion.value = dolares * 4050;
+    console.log(conversion.value);
+}
