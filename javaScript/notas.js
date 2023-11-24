8d23ed48d939f763550878f1b7d5bb011e3850a6
Jesus Suarez <jesus_suarez_11@outlook.com>
2023-11-24 12:08:36 -0500

    credas funciones para tachar notas realizadas y borrar texto en creacion de notas, creado filtro para mostrar unicamente notas realizadas

--- javaScript/notas.js
+++ javaScript/notas.js
@@ -3,13 +3,13 @@ let notas = [
         id: 1,
         titulo: 'Organizar armario',
         descripcion: 'Clasificar la ropa por categorías (camisetas, pantalones, vestidos, etc.) y decidir qué prendas donar, tirar o conservar.',
-        realizada: false
+        realizada: true
     },
     {
         id: 2,
         titulo: 'Programar una Cita',
         descripcion: 'Hacer una llamada al consultorio del doctor Martínez para agendar una cita médica para el chequeo anual.',
-        realizada: false
+        realizada: true
     },
     {
         id: 3,
@@ -17,43 +17,87 @@ let notas = [
         descripcion: 'Resolver ejercicios y problemas relacionados con cada materia. Repasar apuntes y materiales del curso para comprender mejor los conceptos. ',
         realizada: false
     },
+    {
+        id: 4,
+        titulo: 'Hacer ejercicio',
+        descripcion: 'Realizar una sesión de ejercicio cardiovascular y estiramientos durante al menos 30 minutos.',
+        realizada: true
+    },
+    {
+        id: 5,
+        titulo: 'Comprar comestibles',
+        descripcion: 'Hacer la lista de los alimentos necesarios y dirigirse al supermercado para comprar lo necesario para la semana.',
+        realizada: false
+    },
+    {
+        id: 6,
+        titulo: 'Llamar a mamá',
+        descripcion: 'Realizar una llamada a mamá para saludarla y preguntarle cómo ha estado.',
+        realizada: false
+    },
+    {
+        id: 7,
+        titulo: 'Preparar presentación',
+        descripcion: 'Elaborar diapositivas y material necesario para la presentación en la reunión del trabajo.',
+        realizada: false
+    }
 ]
-
-let idGlobal = 4
+let switchNotasRealizadas = false
+let idGlobal = 8
 
 //pintar notas
 let tarjetas = document.getElementById("tarjetasNotas")
 document.addEventListener('DOMContentLoaded', pintarNotas(notas))
+
 function pintarNotas(notas) {
     tarjetas.innerHTML = ''
-    if (notas.length == 0) {
-        let card = document.createElement("div")
-        card.classList.add("col-12")
-        card.innerHTML =
-                '<h2 class = "text-center text-secondary my-5">No hay elementos para mostrar</h2>'
-        tarjetas.appendChild(card)
+    if (notas.length === 0) {
+        let element = document.createElement("div")
+        element.classList.add("col-12")
+        element.innerHTML =
+            '<h2 class = "text-center text-secondary my-5">No hay elementos para mostrar</h2>'
+        tarjetas.appendChild(element)
     } else {
-        for (let i = 0; i < notas.length; i++) {
-            let card = document.createElement("div")
-            card.classList.add("card", "col-md-4", "col-lg-3", "my-4", "mx-md-5", "mx-lg-4", "p-0")
-            card.innerHTML =
-                `<div class="card-header d-flex justify-content-between bg-dark-subtle align-items-center w-100" >
-        <input class="form-check-input" type="checkbox" value="" id="checkNota">
-        <h5 class="m-2">${notas[i].titulo}</h5>
-        <a class="bi bi-trash3 text-danger clickeable" onClick = "eliminarNota(${notas[i].id})"></a>
-    </div>
-    <div class="card-body">
-        <p class="card-text">
-            ${notas[i].descripcion}
-        </p>
-    </div>`
-            tarjetas.appendChild(card)
+        //Todas las Notas
+        if (switchNotasRealizadas === false) {
+            for (let i = 0; i < notas.length; i++) {
+                let card = document.createElement("div")
+                card.classList.add("card", "col-md-4", "col-lg-3", "my-4", "mx-md-5", "mx-lg-4", "p-0")
+                let descripcionTachada = notas[i].realizada ? 'descripcionTachada' : '';
+                card.innerHTML =
+                    `<div class="card-header d-flex justify-content-between bg-dark-subtle align-items-center w-100" data-id="${notas[i].id}">
+                         <input onClick = "marcarRealizada(${notas[i].id})" type="checkbox" ${notas[i].realizada ? "checked" : ""}> </input>
+                         <h5 class="m-2">${notas[i].titulo}</h5>
+                         <a class="bi bi-trash3 text-danger clickeable" onClick = "eliminarNota(${notas[i].id})"></a>
+                     </div>
+                     <div class="card-body">
+                        <p class="card-text ${descripcionTachada}">${notas[i].descripcion}</p>
+                    </div>`
+                tarjetas.appendChild(card)
+            }
+        // Notas Realizadas
+        } else {
+            let notasRealizadas = notas.filter(nota => nota.realizada === true);
+            for (let i = 0; i < notasRealizadas.length; i++) {
+                let card = document.createElement("div")
+                card.classList.add("card", "col-md-4", "col-lg-3", "my-4", "mx-md-5", "mx-lg-4", "p-0")
+                let descripcionTachada = notasRealizadas[i].realizada ? 'descripcionTachada' : '';
+                card.innerHTML =
+                    `<div class="card-header d-flex justify-content-between bg-dark-subtle align-items-center w-100" data-id="${notasRealizadas[i].id}">
+                         <input onClick = "marcarRealizada(${notasRealizadas[i].id})" type="checkbox" ${notasRealizadas[i].realizada ? "checked" : ""}> </input>
+                         <h5 class="m-2">${notasRealizadas[i].titulo}</h5>
+                         <a class="bi bi-trash3 text-danger clickeable" onClick = "eliminarNota(${notasRealizadas[i].id})"></a>
+                     </div>
+                     <div class="card-body">
+                        <p class="card-text ${descripcionTachada}">${notasRealizadas[i].descripcion}</p>
+                    </div>`
+                tarjetas.appendChild(card)
+            }
         }
     }
 }
 
 //Crear nueva Nota
-
 let guardar = document.getElementById('guardar')
 guardar.addEventListener('click', crearNota)
 
@@ -72,6 +116,16 @@ function crearNota() {
     pintarNotas(notas)
 }
 
+//Borrar texto
+let borrarTexto = document.getElementById('borrar')
+borrarTexto.addEventListener('click', e => {
+    let titulo = document.getElementById('tituloNota')
+    let descripcion = document.getElementById('textoNota')
+    titulo.value = ""
+    descripcion.value = ""
+})
+
+//Eliminar Nota
 function eliminarNota(id) {
     for (let i = 0; i < notas.length; i++) {
         if (notas[i].id == id) {
@@ -80,3 +134,26 @@ function eliminarNota(id) {
     }
     pintarNotas(notas)
 }
+//"Realizar" Nota
+function marcarRealizada(id) {
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i].id == id) {
+            if (notas[i].realizada === false) {
+                notas[i].realizada = true
+            } else {
+                notas[i].realizada = false
+            }
+        }
+    }
+    pintarNotas(notas)
+}
+
+//usar filter para filtrar notas con actividades ya hechas
+
+function mostrarRealizadas() {
+    switchNotasRealizadas = !switchNotasRealizadas;
+    console.log(switchNotasRealizadas);
+    pintarNotas(notas);
+}
+
+
